eb053401dc0d735cd3989e15b0e2351a3736601a
Eirik Torske <eirik.torske@axess.no>
2012-03-09 12:39:32 +0100

    Necessary IIS config (TODO: add 'web.config' from HTML5 Boilerplate)

--- Backboneapp/deploy.cmd
+++ Backboneapp/deploy.cmd
@@ -1,7 +1,12 @@
 @ECHO OFF
 @SETLOCAL
 
-XCOPY /Y /F .\backboneapp.htm C:\Temp\MyBackboneApp
-XCOPY /Y /F .\Backboneapp_data.json C:\Temp\MyBackboneApp
+RMDIR /S /Q C:\Temp\MyBackboneApp
+
+MKDIR C:\Temp\MyBackboneApp
+
+XCOPY /Y /F .\web.config C:\Temp\MyBackboneApp\
+XCOPY /Y /F .\backboneapp.htm C:\Temp\MyBackboneApp\
+XCOPY /Y /F .\Backboneapp_data.json C:\Temp\MyBackboneApp\
 XCOPY /Y /S /I /F .\Scripts C:\Temp\MyBackboneApp\Scripts
 XCOPY /Y /S /I /F .\Styles C:\Temp\MyBackboneApp\Styles
