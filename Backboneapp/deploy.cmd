0e0d0f06001fc9f466baa88d45c53c25229ba04f
Eirik Torske <eirik.torske@axess.no>
2012-03-09 12:01:15 +0100

    Very simple deploy BAT script added (local hardcoded PATH)

---
+++ Backboneapp/deploy.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+@SETLOCAL
+
+XCOPY /Y /F .\backboneapp.htm C:\Temp\MyBackboneApp
+XCOPY /Y /F .\Backboneapp_data.json C:\Temp\MyBackboneApp
+XCOPY /Y /S /I /F .\Scripts C:\Temp\MyBackboneApp\Scripts
+XCOPY /Y /S /I /F .\Styles C:\Temp\MyBackboneApp\Styles
