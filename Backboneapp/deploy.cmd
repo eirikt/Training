c948327269fd2940497c89fcc27662b968531b05
Eirik Torske <eirik.torske@gmail.com>
2012-03-09 12:51:33 +0100

    'backboneapp.htm' -> 'index.html'

--- Backboneapp/deploy.cmd
+++ Backboneapp/deploy.cmd
@@ -6,7 +6,7 @@ RMDIR /S /Q C:\Temp\MyBackboneApp
 MKDIR C:\Temp\MyBackboneApp
 
 XCOPY /Y /F .\web.config C:\Temp\MyBackboneApp\
-XCOPY /Y /F .\backboneapp.htm C:\Temp\MyBackboneApp\
+XCOPY /Y /F .\index.html C:\Temp\MyBackboneApp\
 XCOPY /Y /F .\Backboneapp_data.json C:\Temp\MyBackboneApp\
 XCOPY /Y /S /I /F .\Scripts C:\Temp\MyBackboneApp\Scripts
 XCOPY /Y /S /I /F .\Styles C:\Temp\MyBackboneApp\Styles
