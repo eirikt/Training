6be05569440fe3a739b353713928c32ad5436581
Eirik Torske <eirik.torske@gmail.com>
2012-03-20 11:33:13 +0100

    Styling re-added

---
+++ Backboneapp/js/my-templates.js
@@ -0,0 +1,12 @@
+﻿//////    Underscore.js templates    //////
+var menuBackboneViewCollectionTemplate =
+    "<% _.each(data, function(item) { %>" +
+        "<div id='<%= item.attributes.position %>' class='menu-item'>" +
+        "<a href='#info/<%= item.attributes.position %>' class='title'><%= item.attributes.title %></a>" +
+        "</div>" +
+        "<% }); %>";
+
+var contentBackboneViewTemplate =
+    "<div id='<%= attributes.position %>' class='content'>" +
+        "<%= attributes.content %>" +
+        "</div>";
