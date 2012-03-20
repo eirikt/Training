555db8c6c9916817b3a9385f94463381983cba1b
Eirik Torske <eirik.torske@gmail.com>
2012-03-20 11:29:05 +0100

    View rewritten into modern JsTestDriver test layout

--- Backboneapp/test/backbone-view-test.js
+++ Backboneapp/test/backbone-view-test.js
@@ -1,50 +1,37 @@
-function setUp() {
-    /*:DOC +=
-     <script type="text/template" id="content-backbone-view-template">
-     <div id="<%= attributes.position %>" class="content">
-     <%= attributes.content %>
-     </div>
-     </script>*/
+BackboneViewTest = TestCase("BackboneViewTest");
 
+BackboneViewTest.prototype.setUp = function () {
     /*:DOC +=
-     <div id="content">
-     <div id="backbone-menu"></div>
      <div id="backbone-content">Hello World</div>
-     </div>*/
+     */
+};
 
-    this.tabs = window.document.getElementById("tabs");
-}
+BackboneViewTest.prototype.test1 = function () {
+    jstestdriver.console.log("Rendering of 'ContentView with Underscore.js templates");
+    var MyItem = Backbone.Model.extend({
+        "position": 0,
+        "title": "",
+        "content": ""
+    });
 
+    var data = new MyItem();
+    data.set({position: 18});
+    data.set({content: "My content"});
 
-TestCase("BackboneViewTest", {
-    setUp: function () {
-        setUp.call(this);
-    },
+    var defaultView = new ContentView({ model: data});
+    assertObject(defaultView);
+    assertInstanceOf(ContentView, defaultView);
 
+    defaultView.render();
 
-    "test.1: Rendering of ContentView": function () {
-        var Itemm = Backbone.Model.extend({
-            "position": 0,
-            "title": "",
-            "content": ""
-        });
+    assertEquals("<div id=\"18\" class=\"content\">My content</div>", $('#backbone-content').html());
+};
 
-        var data = new Itemm();
-        data.set({position: 18});
-        data.set({content: "My content"});
+BackboneViewTest.prototype.test2 = function () {
+    jstestdriver.console.log("Rendering of 'ContentView' with Underscore.js templates and with Item model");
+    new ContentView({ model: new Item({position: 18, content: "My content"})}).render();
+    assertEquals("<div id=\"18\" class=\"content\">My content</div>", $('#backbone-content').html());
+};
 
-        var defaultView = new ContentView({ model: data});
-        assertObject(defaultView);
-        assertInstanceOf(ContentView, defaultView);
-
-        defaultView.render();
-
-        assertEquals("\n     <div id=\"18\" class=\"content\">\n     My content\n     </div>\n     ", $('#backbone-content').html());
-    },
-
-
-    "test.2: Rendering of ContentView with Item model": function () {
-        new ContentView({ model: new Item({position: 18, content: "My content"})}).render();
-        assertEquals("\n     <div id=\"18\" class=\"content\">\n     My content\n     </div>\n     ", $('#backbone-content').html());
-    }
-});
+BackboneViewTest.prototype.tearDown = function () {
+};
